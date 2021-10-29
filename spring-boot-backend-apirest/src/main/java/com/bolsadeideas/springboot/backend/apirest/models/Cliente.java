4db724a52db304823b0bfb0497d313f99294538f
dpulid1 <diegopulido-951@hotmail.com>
2021-10-29 09:54:27 -0500

    Actualizacion V3.1

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/Cliente.java
@@ -13,6 +13,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "clientes")
@@ -21,9 +22,12 @@ public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
     @Column(nullable = false)
     private String nombre;
     private String apellidos;
+
     @Column(nullable = false, unique = true)
     private String email;
 
