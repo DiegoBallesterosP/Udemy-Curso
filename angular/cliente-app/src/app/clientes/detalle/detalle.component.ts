ff2c924496a4165effac3dd43269c6af7ca1d724
dpulid1 <diegopulido-951@hotmail.com>
2021-11-05 08:55:17 -0500

    actualizacion v6

--- angular/cliente-app/src/app/clientes/detalle/detalle.component.ts
+++ angular/cliente-app/src/app/clientes/detalle/detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 import swal from 'sweetalert2';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
@@ -13,7 +14,7 @@ export class DetalleComponent implements OnInit {
 
   cliente: Cliente;
   titulo: string ="Detalle del cliente";
-  private fotoSeleccionada: File;
+  public fotoSeleccionada: File;
   constructor(private clienteService: ClienteService, 
     private activateRoute: ActivatedRoute) { }
 
@@ -30,14 +31,23 @@ export class DetalleComponent implements OnInit {
   seleccionarFoto(event){
 this.fotoSeleccionada= event.target.files[0];
 console.log(this.fotoSeleccionada);
+if(this.fotoSeleccionada.type.indexOf('image')< 0){
+  swal('Error seleccionar imagen:', 'El archivo debe ser una imagen', 'error');
+  this.fotoSeleccionada=null;
+}
   }
 
 subirFoto(){
+if(!this.fotoSeleccionada){
+  swal('Error Upload:', 'Debe seleccionar una foto', 'error');
+}else{
+
   this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
   .subscribe(cliente => {
     this.cliente = cliente;
     swal('La foto se ha subido completamente!', `la foto se ha subido con exito: ${this.cliente.foto}`, 'success');
     });
+}
 }
 
 }
